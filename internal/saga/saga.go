2d9d7525ade051225ebb1572c817d8010bc239d6
Mateus Mendonça <mateus.mlo95@gmail.com>
2026-01-29 15:44:42 -0300

    feat(saga): adds initial saga structs

--- internal/saga/saga.go
+++ internal/saga/saga.go
@@ -1,18 +1,58 @@
 package saga
 
 import (
-	"encoding/json"
 	"time"
 
+	"github.com/bytedance/sonic"
 	"github.com/google/uuid"
 )
 
 type SagaState struct {
-	SagaID      uuid.UUID       `json:"saga_id"`
-	OrderID     uuid.UUID       `json:"order_id"`
-	Status      SagaStatus      `json:"status"`
-	CurrentStep SagaStep        `json:"current_step"`
-	Payload     json.RawMessage `json:"payload"`
-	StartedAt   time.Time       `json:"started_at"`
-	UpdatedAt   time.Time       `json:"updated_at"`
+	SagaID      uuid.UUID              `json:"saga_id"`
+	OrderID     uuid.UUID              `json:"order_id"`
+	Status      SagaStatus             `json:"status"`
+	CurrentStep SagaStep               `json:"current_step"`
+	Payload     sonic.NoCopyRawMessage `json:"payload"`
+	StartedAt   time.Time              `json:"started_at"`
+	UpdatedAt   time.Time              `json:"updated_at"`
+}
+
+type SagaWorkflow struct {
+	Steps []StepDefinition
+}
+
+type StepDefinition struct {
+	Step             SagaStep
+	CommandTopic     string
+	ReplyTopic       string
+	CompensationStep *SagaStep
+}
+
+func GetOrderWorkflow() *SagaWorkflow {
+	return &SagaWorkflow{
+		Steps: []StepDefinition{
+			{
+				Step:             StepReserveInventory,
+				CommandTopic:     "inventory.commands",
+				ReplyTopic:       "inventory.replies",
+				CompensationStep: nil,
+			},
+			{
+				Step:             StepProcessPayment,
+				CommandTopic:     "payment.commands",
+				ReplyTopic:       "payment.replies",
+				CompensationStep: ptrTo(StepCompensateInventory),
+			},
+			{
+				Step:             StepSendNotification,
+				CommandTopic:     "notification.commands",
+				ReplyTopic:       "notification.replies",
+				CompensationStep: ptrTo(StepCompensatePayment),
+			},
+		},
+	}
+}
+
+func ptrTo[T any](value T) *T {
+	return &value
 }
--- internal/saga/saga_enums.go
+++ internal/saga/saga_enums.go
@@ -10,13 +10,10 @@ const (
 	SagaStatusCancelled   SagaStatus = "CANCELLED"
 	SagaStatusFailed      SagaStatus = "FAILED"
 	SagaStatusCompensated SagaStatus = "COMPENSATED"
-)
 
-const (
-	StepReserve           SagaStep = "RESERVE_INVENTORY"
-	StepProcessPayment    SagaStep = "PROCESS_PAYMENT"
-	StepSendNotification  SagaStep = "SEND_NOTIFICATION"
-	StepConfirmOrder      SagaStep = "CONFIRM_ORDER"
-	StepComplete          SagaStep = "COMPLETE"
-	StepCompensatePayment SagaStep = "COMPENSATE_PAYMENT"
+	StepReserveInventory    SagaStep = "RESERVE_INVENTORY"
+	StepProcessPayment      SagaStep = "PROCESS_PAYMENT"
+	StepSendNotification    SagaStep = "SEND_NOTIFICATION"
+	StepCompensatePayment   SagaStep = "COMPENSATE_PAYMENT"
+	StepCompensateInventory SagaStep = "COMPENSATE_INVENTORY"
 )
